03c65770d03d5887eaf3248065ea5f779128a377
Abdullah Ijaz <98353306+abdullahpichoo@users.noreply.github.com>
2023-06-22 16:34:08 +0500

    swiper done

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -2,18 +2,35 @@ import React from "react";
 import ReviewCard from "./ReviewCard";
 import { reviewsList } from "../../commons/reviewsList";
 
+// Swiper
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+
 const Reviews = () => {
   return (
     <section className="container py-5">
-      {reviewsList.map((review) => (
-        <ReviewCard
-          key={review.name}
-          image={review.image}
-          name={review.name}
-          designation={review.designation}
-          body={review.body}
-        />
-      ))}
+      <Swiper
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {reviewsList.map((review) => (
+          <SwiperSlide>
+            <ReviewCard
+              key={review.name}
+              image={review.image}
+              name={review.name}
+              designation={review.designation}
+              body={review.body}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 };
