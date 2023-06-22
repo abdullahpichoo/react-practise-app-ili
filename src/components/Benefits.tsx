4b9f254c98c51b99afbd349d2db8c8a19aa8982c
Abdullah Ijaz <98353306+abdullahpichoo@users.noreply.github.com>
2023-06-22 15:50:03 +0500

    added image in benefits

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -13,7 +13,7 @@ const Benefit = ({
     <div className="d-flex align-items-center gap-3">
       <div>
         <i
-          className={`bi bi-${icon} rounded-icon py-3 px-4 text-blue fs-1`}
+          className={`bi bi-${icon} rounded-icon py-3 px-4 text-blue fs-3`}
         ></i>
       </div>
       <div className="details">
@@ -27,32 +27,38 @@ const Benefit = ({
 const Benefits = () => {
   return (
     <section className="container d-flex flex-column justify-content-center align-items-center">
-      <h3 className="text-dark-blue fw-bold">
-        Enim quis est voluptatibus aliquid consequatur fugiat
-      </h3>
-      <p className="text-secondary text-center">
-        Esse voluptas cumque vel exercitationem. Reiciendis est hic accusamus.
-        Non ipsam et sed minima temporibus laudantium. Soluta voluptate sed
-        facere corporis dolores excepturi. Libero laboriosam sint et id nulla
-        tenetur. Suscipit aut voluptate.
-      </p>
-
-      <div className="d-flex flex-column gap-5 mt-5">
-        <Benefit
-          title="Lorem Ipsum"
-          desc="Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"
-          icon="fingerprint"
-        />
-        <Benefit
-          title="Nemo Enim"
-          desc="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque"
-          icon="gift"
-        />
-        <Benefit
-          title="Dine Pad"
-          desc="Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis"
-          icon="flower2"
-        />
+      <div className="row align-items-center benefits-section g-5">
+        <div className="col-12 col-lg-5 bg-blue benefits-img-container">
+          <p></p>
+        </div>
+        <div className="col col-lg-7">
+          <h3 className="text-dark-blue fw-bold">
+            Enim quis est voluptatibus aliquid consequatur fugiat
+          </h3>
+          <p className="text-secondary ">
+            Esse voluptas cumque vel exercitationem. Reiciendis est hic
+            accusamus. Non ipsam et sed minima temporibus laudantium. Soluta
+            voluptate sed facere corporis dolores excepturi. Libero laboriosam
+            sint et id nulla tenetur. Suscipit aut voluptate.
+          </p>
+          <div className="d-flex flex-column gap-5 mt-5">
+            <Benefit
+              title="Lorem Ipsum"
+              desc="Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"
+              icon="fingerprint"
+            />
+            <Benefit
+              title="Nemo Enim"
+              desc="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque"
+              icon="gift"
+            />
+            <Benefit
+              title="Dine Pad"
+              desc="Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis"
+              icon="flower2"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
