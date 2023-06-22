5a8e8637982ff39391fa930041103ee5e4bae0b1
Abdullah Ijaz <98353306+abdullahpichoo@users.noreply.github.com>
2023-06-22 12:05:04 +0500

    fixed the SectionHeading underline

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const SectionHeading = ({ title, desc }: { title: string; desc: string }) => {
   return (
     <div className="container text-center d-flex flex-column align-items-center">
-      <h2 className="section-header fw-bold">
+      <h2 className="section-header fw-bold d-flex flex-column justify-content-center align-items-center">
         {title}
         <p>
-          <p></p>
+          <span></span>
         </p>
       </h2>
 
